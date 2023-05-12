bae49bb0b714eb19cbc73cf3e9d06d60b9693301
STAY-TaeHui <masta95@naver.com>
2023-05-13 00:18:53 +0900

    힙 - 디스크 컨트롤러

--- src/힙/디스크컨트롤러/Main.java
+++ src/힙/디스크컨트롤러/Main.java
@@ -12,7 +12,8 @@ public class Main
     public static void main(String[] args)
     {
 //[[0, 3], [1, 9], [2, 6]]
-        Solution.solution(new int[][]{ {1, 2}, {2, 7}, {0, 5}, {3, 3}});
+//        Solution.solution(new int[][]{ {1, 2}, {2, 7}, {0, 5}, {3, 3}});
+        Solution.solution(new int[][]{ {0,3}, {1,9}, {2,6}});
     }
 }
 /*
@@ -20,28 +21,31 @@ public class Main
 * */
 class Solution {
     public static int solution(int[][] jobs) {
-        int answer = 0;
-        Arrays.sort(jobs);
+        Arrays.sort(jobs, (o1,o2)->o1[0]-o2[0]);
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((c1, c2)-> (c1[1]-c2[1]));
-        pq.add(jobs[0]);// 0초에 바로 넣기
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
 
-        int time = 0; //총 걸린 시간
-        int endTime = 0;
+        int totalEndTime = 0; //총 요청~종료 시간 합계
+        int endTime =0; // 이전 작업 끝난 시간
+        int job =0; //작업 횟수
+        int i = 0; // index
 
-        for(int i=0; i< jobs.length; i++){
-            while(!pq.isEmpty()){
-                int[] nextJob = pq.remove();
-                time +=nextJob[1];
-                endTime +=nextJob[1];
+        while(job< jobs.length){ // 총 작업 횟수만큼 반복
+            while(i<jobs.length && jobs[i][0] <= endTime){ //
+                pq.add(jobs[i++]);
             }
 
-
-
+            if(pq.isEmpty()){
+                endTime = jobs[i][0];
+            }
+            else{
+                int[] nextJob = pq.remove();
+                totalEndTime += endTime +  nextJob[1] - nextJob[0];
+                endTime += nextJob[1];
+                job++;
+            }
         }
 
-
-
-        return answer;
+        return totalEndTime/ jobs.length;
     }
 }
