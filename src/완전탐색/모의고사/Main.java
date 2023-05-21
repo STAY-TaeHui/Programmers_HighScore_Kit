7f02406125772913a7322020085dd436494b1a90
thseo <thseo@spectra.co.kr>
2023-05-21 14:31:27 +0900

    완전탐색 - 모의고사

---
+++ src/완전탐색/모의고사/Main.java
@@ -0,0 +1,20 @@
+package 완전탐색.모의고사;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/42840
+public class Main
+{
+    public static void main(String[] args)
+    {
+    }
+}
+class Solution {
+    public int[] solution(int[] answers) {
+        int[] answer = {};
+        return answer;
+    }
+}
