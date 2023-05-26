55adcba42327d162a79759d29f2efe98b64c1d32
thseo <thseo@spectra.co.kr>
2023-05-26 11:09:38 +0900

    깊이너비우선탐색 - 타겟넘버 - DFS와 Stack으로 구현

--- src/깊이너비우선탐색/타겟넘버/Main.java
+++ src/깊이너비우선탐색/타겟넘버/Main.java
@@ -12,18 +12,56 @@ public class Main
 }
 class Solution {
     static int answer = 0;
+    static Stack<Integer> st = new Stack<>();
+
     public static int solution(int[] numbers, int target) {
-        dfs(0,  numbers, target,0);
+
+        dfs(0, numbers, target);
 
         return answer;
     }
-    public static void dfs(int depth, int [] numbers, int target, int sum){
-        if (depth == numbers.length){
-            if(sum==target) answer++;
-        }else{
-            dfs(depth+1, numbers,target,sum+numbers[depth]);
-            dfs(depth+1, numbers,target,sum-numbers[depth]);
+    /*
+    * depth : 재귀 호출 횟수 -> numbers의 index로 사용
+    * numbers
+    * target
+    * */
+    public static void dfs(int depth, int[] numbers, int target){
+        if(numbers.length==st.size()){ // 스택이 꽉 차면, SUM 구해준다.
+            int sum = st.stream().mapToInt(Integer::intValue).sum();
+            if(target == sum){
+                System.out.println(depth +" " + target + " " + sum);
+                answer++;
+            }
+        }
+        else{
+            st.push(numbers[depth]); // 양수일 때 push
+            dfs(depth + 1, numbers, target);
+            st.push(st.pop() * -1);// 음수 일 때 push
+            dfs(depth + 1, numbers, target);
 
+            st.pop();// 한 숫자에 대한 양수/음수 push 가 끝나면 pop
         }
     }
 }
+/*
+0 1 2 3
+4
+  1
+    2
+       1  --> SUM 계산
+      -1  --> SUM 계산
+
+    -2
+       1  --> SUM 계산
+      -1  --> SUM 계산
+
+  -1
+     2
+        1  --> SUM 계산
+       -1  --> SUM 계산
+
+    -2
+        1  --> SUM 계산
+       -1  --> SUM 계산
+
+* */
